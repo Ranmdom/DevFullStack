b6b6ed663cce6d8ddd0223fc01a56842bed39c47
Ranmdom <slimegamer12@gmail.com>
2023-05-19 20:44:36 -0300

    Programa de Dobro de número

---
+++ Atividade2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite um número: ");
+string input = Console.ReadLine();
+
+
+int numero;
+//Converter o string em número inteiro
+//Verificação se é um numero inteiro ou não 
+bool  sucesso = int.TryParse(input, out numero);
+if(sucesso){
+    int dobro = numero * 2; 
+    Console.WriteLine("O dobro do número digitado é: " + dobro);
+}
+else{
+    Console.WriteLine("Entrada incorreta, Só é possível digitar números inteiros");
+}
+
+
+
+
+
+
+
+

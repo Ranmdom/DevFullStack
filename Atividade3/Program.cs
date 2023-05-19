915ef444a9581896d376f42c9ba068a953b70cee
Ranmdom <slimegamer12@gmail.com>
2023-05-19 20:50:45 -0300

    Programa de metros para centimetros

---
+++ Atividade3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Pedir a entrada em metros
+Console.WriteLine("Digite o número em metros: ");
+string input = Console.ReadLine();
+//Metros para centrimétros = 1 m x 100 
+float metros; 
+float.TryParse(input, out metros);
+
+float centrimetos = metros * 100;
+Console.WriteLine(metros + " metros em centrimetos é: " + centrimetos + " Centrimetros");

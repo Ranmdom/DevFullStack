46b10c72eade097688621b318f8b59c7604ca123
Ranmdom <slimegamer12@gmail.com>
2023-05-19 20:57:21 -0300

    Programa de área do quadrado

---
+++ Atividade4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Pedir a área do quadrado 
+Console.WriteLine("Digite a dimensão do quadrado: ");
+string input = Console.ReadLine();
+
+//A = b.h ou b.b 
+float b; 
+float.TryParse(input, out b); //Converter a string em número 
+
+float area = b * b;
+Console.WriteLine("A área do quadrado é: " + area);
+

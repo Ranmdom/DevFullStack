69ce92c61d8ce4d054d869a99a276b5cf8aa3b61
Ranmdom <slimegamer12@gmail.com>
2023-05-19 21:17:02 -0300

    Programa de reajuste de salário

---
+++ Atividade7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite o seu salário: ");
+string input = Console.ReadLine();
+
+float salario;
+float.TryParse(input, out salario);
+
+float reajuste = salario * 12/100;
+float novo_salario = salario + reajuste;
+
+Console.WriteLine("Valor do novo salário com 12% de reajuste: " +novo_salario);
+
+//12% = 12/100 

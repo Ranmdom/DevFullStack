cdd9ccb6a4911c6d1177e92c169b52e0287b3145
Ranmdom <slimegamer12@gmail.com>
2023-05-19 21:11:03 -0300

    Programa de antecessor e sucessor

---
+++ Atividade6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Digite um número: ");
+string input = Console.ReadLine();
+
+int numero; 
+int.TryParse(input, out numero);
+
+int antecessor = numero - 1; 
+int sucessor = numero  + 1;
+
+
+Console.WriteLine("Antecessor do valor: " + antecessor);
+Console.WriteLine("Valor do número: " + numero);
+Console.WriteLine("Sucessor do numero: " + sucessor);

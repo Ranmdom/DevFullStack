8848d1cc48ae1ed822f380b5ba40652fb471368a
Ranmdom <slimegamer12@gmail.com>
2023-05-19 21:03:17 -0300

    Programa da área do circulo

---
+++ Atividade5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite o raio do circulo: ");
+string input = Console.ReadLine();
+//Area do circulo = pi * (raio * raio)
+float raio;
+float.TryParse(input, out raio);
+
+float areaC = 3.14f * (raio * raio);
+Console.WriteLine("A área do circulo é: " + areaC + " m²");

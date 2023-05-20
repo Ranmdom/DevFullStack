baca2e65bb535a03d8211e8a9165beab3363ced2
Ranmdom <slimegamer12@gmail.com>
2023-05-19 22:04:10 -0300

    Programa de graus para Fahrenheit

---
+++ Desafios/desafio2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Programa para ler uma temperatura em graus para fahrenheit 
+Console.WriteLine("Digite a quantidade do valor em graus: ");
+string input = Console.ReadLine();
+
+float graus;
+bool sucesso = float.TryParse(input, out graus);
+if(sucesso){
+//Formula C/5 = F - 32 / 9 ou seja multiplicar por 1.8 e somar 32. 
+  float F = (graus * 1.8f) + 32;
+
+  Console.WriteLine("A temperatura em Fahrenheit é: " + F);
+}
+else{
+    Console.WriteLine("Entradad inválida, por favor digite apenas números");
+}
+
+
+

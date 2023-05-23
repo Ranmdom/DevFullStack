30160284cbeccf53d72d7fc0fef1a502793a0754
Ranmdom <slimegamer12@gmail.com>
2023-05-23 11:48:02 -0300

    Programa de calculo de viagem

---
+++ Lista 2/Atividade5/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Programa de calcular tempo de viagem 
+Precisando de velocidade média ( VM ) e distancia em (KM)
+tempo = km / vm 
+*/
+
+Console.WriteLine("Qual a distência que vai percorrer: ");
+string input_D = Console.ReadLine();
+
+Console.WriteLine("Qual a velocidade média: ");
+string input_V = Console.ReadLine();
+
+float KM, VM,tempo; 
+
+bool sucesso_D = float.TryParse(input_D, out KM);
+bool sucesso_V = float.TryParse(input_V, out VM);
+
+if(sucesso_D && sucesso_V){
+    tempo = VM / KM;
+    //calculo para transformar em minutos 
+    tempo = tempo * 100;
+
+    Console.WriteLine("O tempo de viagem será de: " + tempo + " minutos");
+
+}

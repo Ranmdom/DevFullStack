3a4acd5c9c1f69f229115c64f0a6f22ae3f2cf87
Ranmdom <slimegamer12@gmail.com>
2023-05-23 15:55:18 -0300

    Ainda em construção

---
+++ Lista 2/Atividade6/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Programa para calcular a redução de tempo de vida de um fumante
+1- Quantidade de cigarros que ele fuma por dia 
+2 - 1 cigarro = 10 min de vida perdida 
+24h = x minutos 
+mostrar a quantidade de vida ele perderá 
+*/
+
+Console.WriteLine("Hello, World!");

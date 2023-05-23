13a9dddea7fc85773234040ec36450d27ec00085
Ranmdom <slimegamer12@gmail.com>
2023-05-23 10:54:00 -0300

    Programa de dobro da area de um quadrado

---
+++ Lista 2/Atividade3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Programa que calcula a área do quadrado, e depois mostrar o dobro da area 
+Area = b.h ou b.b; 
+*/
+
+Console.WriteLine("Digite o lado do quadrado: ");
+string input = Console.ReadLine();
+
+float area,lado; 
+bool sucesso_A = float.TryParse(input , out lado);
+
+if(sucesso_A = true){
+    area = lado * lado;
+
+    Console.WriteLine("A área do quadrado é: " + area + " m2");
+    Console.WriteLine("O dobro da sua área é: " + area * 2 + " m2"); 
+}
+else{
+    Console.WriteLine("Entrada inválida ,por favor digite apenas números");
+}
+

5c3af2f9be17780995e6a82cce6318047ccb00c4
Ranmdom <slimegamer12@gmail.com>
2023-05-24 16:45:04 -0300

    Programa de Peso

---
+++ Lista 2/Atividade9/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Entrada altura da pessoa 
+Calcular o peso ideai usando a formual (72,7 * altura) - 58
+*/
+Console.WriteLine("Digite a sua altura: ");
+string input = Console.ReadLine();
+
+float altura; 
+bool sucesso_a = float.TryParse(input , out altura);
+
+if(sucesso_a = true){
+    altura = (72.7f * altura) - 58;
+    double  resultado_A = Math.Round(altura, 1);  //Math.round é um arredondamento para 1 casa decimal a ami
+    Console.WriteLine("Seu pedo ideal é: " + resultado_A + " kg");
+
+}

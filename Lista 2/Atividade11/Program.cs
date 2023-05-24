85ce99de08e369fc98d398c467d361ecc24651fb
Ranmdom <slimegamer12@gmail.com>
2023-05-24 18:30:04 -0300

    Começo do programa

---
+++ Lista 2/Atividade11/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Ler o número total de leitores de um munícipio 
+numero de votos brancos, nulos e validos 
+Calcular e escrever o percentual que cada um representa 
+em relação total de leitores 
+*/
+Console.WriteLine("Informe o número total de leitores: ");
+string input = Console.ReadLine();
+
+Console.WriteLine("Informe o núemero de votos brancos: ");
+string inputB = Console.ReadLine();
+
+Console.WriteLine("Informe a quantidade de votos nulos: ");
+string inputN = Console.ReadLine();
+
+int leitores, votos_nulos, votos_brancos;
+
+
+

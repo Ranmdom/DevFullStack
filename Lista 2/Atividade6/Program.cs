e74a10608b4820feefdb1cfa02636774f52cf119
Ranmdom <slimegamer12@gmail.com>
2023-05-24 11:10:02 -0300

    Programa de dias perdidos com cigarro

--- Lista 2/Atividade6/Program.cs
+++ Lista 2/Atividade6/Program.cs
@@ -1,10 +1,20 @@
 ﻿/*
-Programa para calcular a redução de tempo de vida de um fumante
+Programa para calcular a redução de tempo de vida de um fumante 
 1- Quantidade de cigarros que ele fuma por dia 
 2 - 1 cigarro = 10 min de vida perdida 
-24h = x minutos 
+24h = 1440 minutos 
 mostrar a quantidade de vida ele perderá 
 */
+Console.WriteLine("Digite a quantidade cigarros que fuma por dia: ");
+string input_C = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+int cigarros_dia, tempo_de_fumo, vida_perdida;
+
+bool sucesso_C = int.TryParse(input_C, out cigarros_dia);
+if(sucesso_C = true){
+    tempo_de_fumo = cigarros_dia * 10;
+    vida_perdida = tempo_de_fumo / 1440;
+    Console.WriteLine("Você perdeu " +vida_perdida+ " dias de vida fumando cigarro");
+
+}
 

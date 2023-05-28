aa690b5f09ef564329dcbc1f95259637ab58a8c0
Ranmdom <slimegamer12@gmail.com>
2023-05-28 18:46:17 -0300

    Positivo ou negativo

---
+++ Lista 3/Atividade1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Programa que peça o valor e mostre se é positivo ou negativo
+*/
+Console.WriteLine("Digite um valor: ");
+int valor = int.Parse(Console.ReadLine());
+
+if(valor >= 0){
+    Console.WriteLine("O valor: " + valor + " é positivo");
+}
+else{
+    Console.WriteLine("O valor " + valor + " é negativo");
+}

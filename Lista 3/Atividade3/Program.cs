7a0f22b68a59a1ca5309fa43ffc7973cdde8e4eb
Ranmdom <slimegamer12@gmail.com>
2023-05-28 19:41:21 -0300

    Atividade 3

---
+++ Lista 3/Atividade3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Programa que verifica se uma letra digitada é "F" ou "M" 
+F - Feminino 
+M - Masculino 
+Se não for uma dessas letras sexo inválido
+*/
+Console.WriteLine("F - Feminino\nM - Masculino");
+Console.WriteLine("Digite a Letra do sexo: ");
+char letra = char.Parse(Console.ReadLine());
+
+if(letra == 'F' || letra == 'f'){
+    Console.WriteLine("Sexo Feminino");
+
+}else if(letra == 'm' || letra == 'M'){
+    Console.WriteLine("Sexo Masculino");
+}else{
+    Console.WriteLine("Sexo inválido");
+}

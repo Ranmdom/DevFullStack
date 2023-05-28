50e03b8a9529a6c3138bc434277d8ac122f56584
Ranmdom <slimegamer12@gmail.com>
2023-05-28 19:59:15 -0300

    Maior e menor

---
+++ Lista 3/Atividade5/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Programa que leia três números e mostre o maior deles
+Apenas números diferentes
+*/ 
+Console.WriteLine("Digite o primeiro número: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o terceiro número: ");
+int numero3 = int.Parse(Console.ReadLine());
+
+int maior = 0;
+
+//Verificar se o número é diferente
+
+if(numero1 != numero2 && numero1 != numero3 && numero2 != numero3){
+     if(numero1 > maior){
+        maior = numero1;
+     }
+
+    if(numero2 > maior){
+        maior = numero2;
+    }
+    if(numero3 > maior){
+        maior = numero3;
+    }
+
+    Console.WriteLine("O maior número é: " + maior);
+
+}
+else{
+    Console.WriteLine("Apenas números diferentes");
+}

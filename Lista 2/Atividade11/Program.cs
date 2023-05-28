477e9f99f132029e2446c001186ace3c36e6dd62
Ranmdom <slimegamer12@gmail.com>
2023-05-28 17:56:22 -0300

    Contagem de votos

--- Lista 2/Atividade11/Program.cs
+++ Lista 2/Atividade11/Program.cs
@@ -3,6 +3,8 @@ Ler o número total de leitores de um munícipio
 numero de votos brancos, nulos e validos 
 Calcular e escrever o percentual que cada um representa 
 em relação total de leitores 
+
+calculo = numero / 100 que vai dar a porcentagem
 */
 Console.WriteLine("Informe o número total de leitores: ");
 string input = Console.ReadLine();
@@ -13,7 +15,21 @@ string inputB = Console.ReadLine();
 Console.WriteLine("Informe a quantidade de votos nulos: ");
 string inputN = Console.ReadLine();
 
-int leitores, votos_nulos, votos_brancos;
+float leitores, votos_nulos, votos_brancos;
 
+bool sucesso_leitores = float.TryParse(input, out leitores);
+bool sucesso_brancos = float.TryParse(inputB, out votos_brancos);
+bool sucesso_nulos = float.TryParse(inputN, out votos_nulos);
+
+if(sucesso_brancos && sucesso_leitores && sucesso_nulos){
+    
+    float P_brancos = votos_brancos / leitores * 100;
+    float P_nulos = votos_nulos / leitores * 100;
+
+    Console.WriteLine("O total de leitores foi: " + leitores);
+    Console.WriteLine("O percentual de votos brancos: " + P_brancos + "%");
+    Console.WriteLine("O percentual de votos brancos: " + P_nulos + "%");
+
+}
 
 

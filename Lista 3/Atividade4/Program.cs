a22a2d8343dc7ace1e2ae063de48798778a0cf01
Ranmdom <slimegamer12@gmail.com>
2023-05-28 19:48:47 -0300

    Vogal ou cosoante

---
+++ Lista 3/Atividade4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Programa que identifica se a letra digitada é vogal ou consoante
+Console.WriteLine("Digite um letra: ");
+char letra = char.Parse(Console.ReadLine());
+
+if(letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U' || letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+{
+        Console.WriteLine(letra + " é uma vogal");
+}else{
+    Console.WriteLine(letra + " é uma consoante");
+}

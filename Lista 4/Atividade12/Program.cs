c803f3d494eb0fed59c46d430b425681859afb20
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:41:08 -0300

    Expoente e base

--- Lista 4/Atividade12/Program.cs
+++ Lista 4/Atividade12/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite a base: ");
+        int baseNum = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o expoente: ");
+        int expoente = int.Parse(Console.ReadLine());
+
+        int resultado = CalcularPotencia(baseNum, expoente);
+
+        Console.WriteLine("{0} elevado a {1} é igual a {2}", baseNum, expoente, resultado);
+    
+
+    static int CalcularPotencia(int baseNum, int expoente)
+    {
+        int resultado = 1;
+
+        for (int i = 1; i <= expoente; i++)
+        {
+            resultado *= baseNum;
+        }
+
+        return resultado;
+    }

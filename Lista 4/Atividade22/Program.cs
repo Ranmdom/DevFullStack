1b3a7540a86a64bbc26ee50f0871fa966c2c920a
Ranmdom <slimegamer12@gmail.com>
2023-06-16 03:02:47 -0300

    Numero primo

--- Lista 4/Atividade22/Program.cs
+++ Lista 4/Atividade22/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite um número inteiro (N): ");
+        int n = int.Parse(Console.ReadLine());
+
+        int countDivisoes = 0;
+
+        Console.WriteLine("Números primos entre 1 e {0}:", n);
+
+        for (int i = 2; i <= n; i++)
+        {
+            bool ehPrimo = true;
+
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                countDivisoes++;
+
+                if (i % j == 0)
+                {
+                    ehPrimo = false;
+                    break;
+                }
+            }
+
+            if (ehPrimo)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        Console.WriteLine("Número de divisões realizadas: {0}", countDivisoes);

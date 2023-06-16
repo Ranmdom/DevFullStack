0e5ad367305a2c6d9fb84ca7de137c124e2a497b
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:25:55 -0300

    Numeros impares

--- Lista 4/Atividade7/Program.cs
+++ Lista 4/Atividade7/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ Console.WriteLine("Números ímpares entre 1 e 50:");
+
+        for (int i = 1; i <= 50; i++)
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+            }
+        }

81723b7f2b3fe44a45dd23babce7c5bb97b51348
Ranmdom <slimegamer12@gmail.com>
2023-06-16 17:17:01 -0300

    Programa de vetores sem funções prontas

--- Lista 5/Atividade10/Program.cs
+++ Lista 5/Atividade10/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿        int tamanhoVetor = 10;
+        double[] vetor = new double[tamanhoVetor];
+
+        Console.WriteLine("Digite os elementos do vetor:");
+
+        for (int i = 0; i < tamanhoVetor; i++)
+        {
+            Console.Write("Elemento {0}: ", i + 1);
+            vetor[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        OrdenarVetor(vetor);
+
+        Console.WriteLine("\nVetor ordenado:");
+
+        for (int i = 0; i < tamanhoVetor; i++)
+        {
+            Console.WriteLine(vetor[i]);
+        }
+
+    static void OrdenarVetor(double[] vetor)
+    {
+        int tamanhoVetor = vetor.Length;
+
+        for (int i = 0; i < tamanhoVetor - 1; i++)
+        {
+            for (int j = 0; j < tamanhoVetor - i - 1; j++)
+            {
+                if (vetor[j] > vetor[j + 1])
+                {
+                    // Troca os elementos de posição
+                    double temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                }
+            }
+        }
+    }

bd988abfa69bc9426d4aab378238201d4047951f
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:52:16 -0300

    Att

--- Lista 4/Atividade18/Program.cs
+++ Lista 4/Atividade18/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Quantos números você deseja digitar? ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        int menor = int.MaxValue;
+        int maior = int.MinValue;
+        int soma = 0;
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            Console.Write("Digite o número {0}: ", i + 1);
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero < menor)
+            {
+                menor = numero;
+            }
+
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+
+            soma += numero;
+        }
+
+        Console.WriteLine("Menor valor: " + menor);
+        Console.WriteLine("Maior valor: " + maior);
+        Console.WriteLine("Soma dos valores: " + soma);

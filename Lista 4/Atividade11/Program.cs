1bc7b210d483d0665f3a1423652cb8029c9d4616
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:37:04 -0300

    Tabuada pelo meio

--- Lista 4/Atividade11/Program.cs
+++ Lista 4/Atividade11/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite um número inteiro para gerar a tabuada: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor inicial da tabuada: ");
+        int inicio = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o valor final da tabuada: ");
+        int fim = int.Parse(Console.ReadLine());
+
+        if (fim >= inicio)
+        {
+            Console.WriteLine("Tabuada do número {0}:", numero);
+
+            for (int i = inicio; i <= fim; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine("{0} x {1} = {2}", numero, i, resultado);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Valor final menor que o valor inicial. Digite um intervalo válido.");
+        }

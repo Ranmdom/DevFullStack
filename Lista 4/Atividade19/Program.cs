14a2cf30241dafc000f6a280d7071eb6ff2e4f87
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:53:01 -0300

    Att do código  anterior

--- Lista 4/Atividade19/Program.cs
+++ Lista 4/Atividade19/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Quantos números você deseja digitar? ");
+        int quantidade = int.Parse(Console.ReadLine());
+
+        int menor = 1001;
+        int maior = -1;
+        int soma = 0;
+
+        for (int i = 0; i < quantidade; i++)
+        {
+            int numero;
+
+            do
+            {
+                Console.Write("Digite o número {0} (entre 0 e 1000): ", i + 1);
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero < 0 || numero > 1000)
+                {
+                    Console.WriteLine("Número inválido. Tente novamente.");
+                }
+            }
+            while (numero < 0 || numero > 1000);
+
+            if (numero < menor)
+            {
+                menor = numero;
+            }
+
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+
+            soma += numero;
+        }
+
+        Console.WriteLine("Menor valor: " + menor);
+        Console.WriteLine("Maior valor: " + maior);
+        Console.WriteLine("Soma dos valores: " + soma);

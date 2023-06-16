3bbe5a3bb85c42b357f06b684d1691ac937224b2
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:25:04 -0300

    Att da quantidade de numeros

--- Lista 4/Atividade6/Program.cs
+++ Lista 4/Atividade6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int quantidadeNumeros = 5;
+        int soma = 0;
+
+        for (int i = 1; i <= quantidadeNumeros; i++)
+        {
+            Console.Write($"Digite o {i}º número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            soma += numero;
+        }
+
+        double media = (double)soma / quantidadeNumeros;
+
+        Console.WriteLine($"A soma dos números é: {soma}");
+        Console.WriteLine($"A média dos números é: {media}");

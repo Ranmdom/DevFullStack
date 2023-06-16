7c11a988a56367e6bc98ca6f6430fb02042bd81c
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:43:26 -0300

    Fibonacci

--- Lista 4/Atividade14/Program.cs
+++ Lista 4/Atividade14/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite o número de termos da série Fibonacci a serem gerados: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int a = 1;
+        int b = 1;
+
+        Console.WriteLine("Série de Fibonacci:");
+
+        Console.Write(a + " ");
+        Console.Write(b + " ");
+
+        for (int i = 3; i <= n; i++)
+        {
+            int c = a + b;
+            Console.Write(c + " ");
+
+            a = b;
+            b = c;
+        }

1f4c6f5905e4a6d4732a32ca7cfa2f7e454afc5e
Ranmdom <slimegamer12@gmail.com>
2023-06-16 17:15:10 -0300

    Programa de matriz

--- Lista 5/Atividade9/Program.cs
+++ Lista 5/Atividade9/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int tamanhoVetor = 10;
+        int[] vetor = new int[tamanhoVetor];
+
+        Console.WriteLine("Digite os elementos do vetor:");
+
+        for (int i = 0; i < tamanhoVetor; i++)
+        {
+            Console.Write("Elemento {0}: ", i + 1);
+            vetor[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nValores iguais encontrados:");
+
+        for (int i = 0; i < tamanhoVetor - 1; i++)
+        {
+            for (int j = i + 1; j < tamanhoVetor; j++)
+            {
+                if (vetor[i] == vetor[j])
+                {
+                    Console.WriteLine(vetor[i]);
+                }
+            }
+        }

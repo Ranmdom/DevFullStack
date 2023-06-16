3262d6d2f46ce959bbc248aff66cc607f7bfeab9
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:45:38 -0300

    Fibonacci 500

--- Lista 4/Atividade15/Program.cs
+++ Lista 4/Atividade15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿        int a = 1;
+        int b = 1;
+
+        Console.WriteLine("Série de Fibonacci:");
+
+        Console.Write(a + " ");
+        Console.Write(b + " ");
+
+        int c = 0;
+
+        while (c < 500)
+        {
+            c = a + b;
+
+            if (c >= 500)
+                break;
+
+            Console.Write(c + " ");
+
+            a = b;
+            b = c;
+        }

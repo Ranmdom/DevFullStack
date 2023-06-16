3475bf66a6de1854dc831370e859995c52843c30
Ranmdom <slimegamer12@gmail.com>
2023-06-16 17:10:38 -0300

    Programa de vetor de 100 primeiros numeros

--- Lista 5/Atividade6/Program.cs
+++ Lista 5/Atividade6/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int tamanhoVetor = 100;
+        int[] vetor = new int[tamanhoVetor];
+        int numero = 1;
+        int contador = 0;
+
+        while (contador < tamanhoVetor)
+        {
+            if (numero % 7 != 0)
+            {
+                vetor[contador] = numero;
+                contador++;
+            }
+            numero++;
+        }
+
+        Console.WriteLine("Vetor com os 100 primeiros números naturais que não são múltiplos de 7:");
+
+        for (int i = 0; i < tamanhoVetor; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }

a0302c30b8b89c8f4b00cf3cf36d714b14f22ee7
Ranmdom <slimegamer12@gmail.com>
2023-06-16 16:56:38 -0300

    Programa de vetores

---
+++ Lista 5/Atividade1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+        double[] vetor = new double[10];
+
+        // Preenchendo o vetor
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Digite o número real {0}: ", i + 1);
+            vetor[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.WriteLine("Vetor na ordem inversa:");
+
+        // Exibindo o vetor na ordem inversa
+        for (int i = 9; i >= 0; i--)
+        {
+            Console.WriteLine(vetor[i]);
+        }

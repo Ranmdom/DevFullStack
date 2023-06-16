81ccd5ae462892ba181fdc08fa3ccbf7178b9c14
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:59:50 -0300

    Divisivel por 1

--- Lista 4/Atividade20/Program.cs
+++ Lista 4/Atividade20/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        bool ehPrimo = VerificarPrimo(numero);
+
+        if (ehPrimo)
+        {
+            Console.WriteLine("{0} é um número primo.", numero);
+        }
+        else
+        {
+            Console.WriteLine("{0} não é um número primo.", numero);
+        }
+    
+
+    static bool VerificarPrimo(int numero)
+    {
+        if (numero < 2)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(numero); i++)
+        {
+            if (numero % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }

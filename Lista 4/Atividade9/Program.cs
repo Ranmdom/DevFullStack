c72555792b4559bccdc84e71912c9bb98c7efd00
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:31:32 -0300

    Att do programa de intervalo

--- Lista 4/Atividade9/Program.cs
+++ Lista 4/Atividade9/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite o primeiro número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Números no intervalo entre {0} e {1}:", numero1, numero2);
+
+        int menor = Math.Min(numero1, numero2);
+        int maior = Math.Max(numero1, numero2);
+        int soma = 0;
+
+        for (int i = menor + 1; i < maior; i++)
+        {
+            Console.WriteLine(i);
+            soma += i;
+        }
+
+        Console.WriteLine("Soma dos números: {0}", soma);

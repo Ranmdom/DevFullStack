e40cd3333d095311e8178d821475931e57f3ade5
Ranmdom <slimegamer12@gmail.com>
2023-06-16 17:12:45 -0300

    Programa de competição de salto

--- Lista 5/Atividade7/Program.cs
+++ Lista 5/Atividade7/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite o nome do atleta: ");
+        string nomeAtleta = Console.ReadLine();
+
+        double[] saltos = new double[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Digite a distância alcançada no {0}º salto: ", i + 1);
+            saltos[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        double media = CalcularMedia(saltos);
+
+        Console.WriteLine("\nResultado final:");
+        Console.WriteLine("Atleta: " + nomeAtleta);
+        Console.Write("Saltos: ");
+        ImprimirVetor(saltos);
+        Console.WriteLine("Média dos saltos: " + media.ToString("F1") + " m");
+
+
+    static double CalcularMedia(double[] vetor)
+    {
+        double soma = 0;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            soma += vetor[i];
+        }
+        return soma / vetor.Length;
+    }
+
+    static void ImprimirVetor(double[] vetor)
+    {
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i].ToString("F1") + " - ");
+        }
+        Console.WriteLine();
+    }

037ad15ded49da9fa13448262a1e9766a0dc85a3
Ranmdom <slimegamer12@gmail.com>
2023-06-16 17:07:28 -0300

    Programa preencher numero de vetor

--- Lista 5/Atividade4/Program.cs
+++ Lista 5/Atividade4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] A = new int[10];
+
+        // Preenchendo o vetor com os números digitados pelo usuário
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Digite o número {0}: ", i + 1);
+            A[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int somaQuadrados = 0;
+
+        // Calculando a soma dos quadrados dos elementos do vetor
+        for (int i = 0; i < 10; i++)
+        {
+            somaQuadrados += A[i] * A[i];
+        }
+
+        Console.WriteLine("A soma dos quadrados dos elementos do vetor é: " + somaQuadrados);

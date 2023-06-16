92235c76676811fec239f8af0b780a3106b74c87
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:49:47 -0300

    Att do fatorial

--- Lista 4/Atividade17/Program.cs
+++ Lista 4/Atividade17/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string opcao;
+        do
+        {
+            Console.Write("Digite um número inteiro positivo menor que 16: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero >= 0 && numero < 16)
+            {
+                int fatorial = 1;
+
+                for (int i = numero; i >= 1; i--)
+                {
+                    fatorial *= i;
+                }
+
+                Console.WriteLine(numero + "! = " + fatorial);
+            }
+            else
+            {
+                Console.WriteLine("Número inválido. Por favor, digite um número inteiro positivo menor que 16.");
+            }
+
+            Console.WriteLine("Deseja calcular o fatorial de outro número? (S/N)");
+            opcao = Console.ReadLine();
+        }
+        while (opcao.ToUpper() == "S");

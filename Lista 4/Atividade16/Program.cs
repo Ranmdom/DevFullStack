befde513511b2eeb55ab8fd4079bd60fedd0d645
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:48:55 -0300

    Fatorial

--- Lista 4/Atividade16/Program.cs
+++ Lista 4/Atividade16/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int fatorial = 1;
+
+        for (int i = numero; i >= 1; i--)
+        {
+            fatorial *= i;
+        }
+
+        Console.WriteLine(numero + "! = " + fatorial);

b24879fccbb7af00fe28edf8652d4c0a25964f1b
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:33:38 -0300

    Tabuada

--- Lista 4/Atividade10/Program.cs
+++ Lista 4/Atividade10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite um número inteiro de 1 a 10: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero >= 1 && numero <= 10)
+        {
+            Console.WriteLine("Tabuada do número {0}:", numero);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine("{0} x {1} = {2}", numero, i, resultado);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Número inválido. Digite um número inteiro de 1 a 10.");
+        }

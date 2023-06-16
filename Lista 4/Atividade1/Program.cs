143921a2ae800339d9466620e1acbfa1069d39aa
Ranmdom <slimegamer12@gmail.com>
2023-06-15 23:32:10 -0300

    Programa de notas

---
+++ Lista 4/Atividade1/Program.cs
@@ -0,0 +1,14 @@
+﻿int nota; 
+
+do{
+    Console.WriteLine("Digite uma nota entre zero e dez: ");
+    nota = int.Parse(Console.ReadLine());
+
+    if(nota < 0 || nota > 10){
+
+        Console.WriteLine("Valor inválido! Digite novamente");
+
+    }
+}while(nota < 0 || nota > 10);
+
+Console.WriteLine("Nota Válida: " + nota);

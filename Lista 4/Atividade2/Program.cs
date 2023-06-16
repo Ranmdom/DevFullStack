db6884fbfa0966d59540f3793b65ce9df8174f35
Ranmdom <slimegamer12@gmail.com>
2023-06-15 23:39:56 -0300

    Programa de crescimento Populacional

--- Lista 4/Atividade2/Program.cs
+++ Lista 4/Atividade2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Declaração de variaveis
+int populacaoA = 80000;
+double taxaCrescimentoA = 0.03;
+int populacaoB = 200000;
+double taxaCrescimentoB = 0.015;
+int anos = 0;
+
+//Estrutura condicional 
+while(populacaoA < populacaoB){
+
+    populacaoA += (int)(populacaoA * taxaCrescimentoA);
+    populacaoB += (int)(populacaoB * taxaCrescimentoB);
+    anos++;
+
+}
+
+Console.WriteLine("Serão necessários " + anos + " anos para que a população do país A ultrapasse ou iguale a população do população do país B.");

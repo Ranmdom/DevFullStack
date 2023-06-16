c9495c526b419d6abd22d17d983a3fa331a6dc10
Ranmdom <slimegamer12@gmail.com>
2023-06-16 02:42:22 -0300

    Sequencia de numeros

--- Lista 4/Atividade13/Program.cs
+++ Lista 4/Atividade13/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite uma sequência de números separados por espaço: ");
+        string entrada = Console.ReadLine();
+
+        string[] numeros = entrada.Split(' ');
+
+        int quantidadePares = 0;
+        int quantidadeImpares = 0;
+
+        foreach (string numeroStr in numeros)
+        {
+            int numero = int.Parse(numeroStr);
+
+            if (numero % 2 == 0)
+            {
+                quantidadePares++;
+            }
+            else
+            {
+                quantidadeImpares++;
+            }
+        }
+
+        Console.WriteLine("Quantidade de números pares: {0}", quantidadePares);
+        Console.WriteLine("Quantidade de números ímpares: {0}", quantidadeImpares);
+    

b54e64973e4ae36281dde6a5af25955e3416c382
Ranmdom <slimegamer12@gmail.com>
2023-06-27 20:16:02 -0300

    Atividade de matriz

--- Lista 6/Atividade3/Program.cs
+++ Lista 6/Atividade3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Leia uma matriz de tamanho 4 x 4. Em seguida, conte e escreva na tela quantos valores 
+maiores do que 10 ela possui. 
+*/
+
+//Declaração das variaveis 
+
+int [,] matriz = new int[4,4];
+
+
+Console.WriteLine("Digite os valores da matriz: ");
+
+for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 4; j++){
+
+        Console.WriteLine($"Digite o valor para a posição [{i}, {j}]: ");
+        matriz[ i, j ] = int.Parse(Console.ReadLine());
+
+    }
+}
+
+//Contagem dos valores maiores que 10
+int count = 0;
+
+for(int i = 0; i < 4; i++){
+
+    for(int j = 0; j < 4; j++){
+
+        if(matriz[i,j] > 10){
+            count++;
+
+        }
+    }
+}
+
+Console.WriteLine($"A matriz possui {count} valores maiores do que 10");

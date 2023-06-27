bbd674eb9260c6ee4177e50cefb71f553f0c5d9c
Ranmdom <slimegamer12@gmail.com>
2023-06-27 20:57:03 -0300

    Código de matriz soma da secundária

--- Lista 6/Atividade5/Program.cs
+++ Lista 6/Atividade5/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Leia uma matriz 3x3. Em seguida, imprima a soma dos valores contidos
+em sua diagonal secundária.
+*/
+
+
+//Declarar variaveis 
+int [,] matriz = new int [3,3];
+int somaDiagonalSecundaria = 0;
+
+//Ler a matriz 
+for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+
+        Console.WriteLine($"Digite o valor da posição [{i},{j}]: ");
+        matriz[i,j] = int.Parse(Console.ReadLine());
+
+    }
+}
+
+//Imprimir a matriz 
+for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+
+        Console.Write(matriz[i,j].ToString().PadLeft(2) + " ");
+
+    }
+
+    Console.WriteLine();
+}
+
+//A soma da diagonal secundária
+for(int j = 0; j < 3; j++){
+
+    somaDiagonalSecundaria += matriz[j,j];
+
+}
+
+Console.WriteLine($"A soma da diagonal secundária: {somaDiagonalSecundaria}");
+

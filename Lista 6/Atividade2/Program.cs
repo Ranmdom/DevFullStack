9c825f218ad2e22d57ad94190b5121d87434d6b7
Ranmdom <slimegamer12@gmail.com>
2023-06-27 21:35:10 -0300

    Atualização do código

--- Lista 6/Atividade2/Program.cs
+++ Lista 6/Atividade2/Program.cs
@@ -1,13 +1,4 @@
-﻿/*
-Faça um programa que declare uma matriz de tamanho 5 x 5. Preencha com 1 a diagonal 
-principal e 0 os demais elementos. Ao final, escreva a matriz obtida na tela.
-
-se i for igual a j então vai ser 1 se não vai ser 0
-
-*/
-
-
-//Declaração de variaveis
+﻿//Declaração de variaveis
 
 int [,] matriz = new int[5,5];
 
@@ -39,7 +30,7 @@ Console.WriteLine("Matriz Resultante: ");
 for(int i = 0; i < 5; i++){
     for(int j = 0; j < 5; j++){
 
-        Console.WriteLine(matriz[i,j].ToString().PadLeft(2) + " "); // Assim fica tudo numa linha só 
+        Console.Write(matriz[i,j].ToString().PadLeft(2) + " "); // Assim fica tudo numa linha só 
     } // PadLeft adiciona espaços a esquerda de cada elemento o PadRight ( na direita)
  Console.WriteLine(" "); // Adicionando coluna
 

36276043554fc254b29cd2fe47c082442139193b
Ranmdom <slimegamer12@gmail.com>
2023-08-23 16:25:21 -0300

    Sistema de contas a pagar

---
+++ Projetos/Contas_A_Pagar/Classes/Escritor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contas_A_Pagar.Classes
+{
+    public class Escritor
+    {
+        
+    }
+}
